531396b8566b9bfff607147adf216b850217eb45
Marcos Tenrero <marcos.95tm@gmail.com>
2017-12-14 12:24:26 +0100

    Fixed Release Script

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ env GOOS=windows GOARCH=amd64 go build -o releases/desbreaker-win-amd64.exe des.
 env GOOS=windows GOARCH=386 go build -o releases/desbreaker-win-i386.exe des.go main.go dic.go
 
 env GOOS=linux GOARCH=amd64 go build -o releases/desbreaker-linux-i386 des.go main.go dic.go
-env GOOS=linux GOARCH=386 go build -o releases/desbreaker-linux-i386 des.go main.go dic.go
+env GOOS=linux GOARCH=386 go build -o releases/desbreaker-linux-amd64 des.go main.go dic.go
 
 env GOOS=darwin GOARCH=amd64 go build -o releases/desbreaker-osx-amd64 des.go main.go dic.go
